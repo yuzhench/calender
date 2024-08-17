076eb3ff9503e34f2c8b27ce9049583cd4585c4b
yuzhen chen <yuzhench@umich.edu>
2024-08-17 19:16:07 -0400

    first update

---
+++ build/CMakeFiles/yuzhench_time_schedule.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yuzhench_time_schedule.dir/src/functionality.cpp.o"
+  "CMakeFiles/yuzhench_time_schedule.dir/src/functionality.cpp.o.d"
+  "yuzhench_time_schedule"
+  "yuzhench_time_schedule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yuzhench_time_schedule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/functionality.cpp
@@ -0,0 +1,344 @@
+#include <ncurses.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <map>
+#include <sstream>
+#include <cstring>
+
+//define the vetor of the tasks: 
+//1. store the information inside the txt file 
+//2. show the welcome page 
+ 
+enum MenuOption {
+    ShowTodayTasks = 0,
+    AddTask,
+    RemoveTask,
+    MarkAsFinished,
+    MarkAsUnfinished,
+    DeleteAll,
+    Leave
+};
+
+char PASSWORD[100] = "cyz030518";
+
+// Function to load tasks from a file
+void load_tasks(std::map<std::string, std::vector<std::string>>& tasks, const std::string& filename, std::string CURR_TIME, WINDOW *menu_win) {
+    std::ifstream file(filename);
+    std::string line;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            std::size_t pos = line.find(':');  // Find the position of the colon
+            if (pos != std::string::npos) {
+                std::string key = line.substr(0, pos);
+                if (key == CURR_TIME){
+                    mvwprintw(menu_win, 20, 1, "load the file successfully ");
+
+                    std::cout<<"this is the correct message" << std::endl;
+                    std::string valueString = line.substr(pos + 1);
+                    std::vector<std::string> values;
+                    std::stringstream ss(valueString);
+                    std::string item;
+                    while (getline(ss, item, ',')) {
+                        values.push_back(item);
+                    }
+                    tasks[key] = values;
+                }
+                 
+            }
+            else{
+                std::cout<<"this is the wrong message" << std::endl;
+            }
+        }
+        file.close();
+    }
+   
+}
+
+// Function to save tasks to a file
+void save_tasks(std::map<std::string, std::vector<std::string>>& tasks,  const std::string& filename) {
+    std::cout<< "start to save the task" << std::endl;
+    std::ofstream file(filename);
+    if (file.is_open()){
+        for (const auto& pair : tasks) {
+            file << pair.first << ":";
+            for (size_t i = 0; i < pair.second.size(); ++i) {
+                file << pair.second[i];
+                if (i < pair.second.size() - 1) {
+                    file << ",";
+                }
+            }
+            file << "\n";
+        }
+    }
+    else if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file for saving tasks." << std::endl;
+    return;
+}
+}
+
+// Function to display the current date
+std::string get_current_date() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    char date[10];
+    sprintf(date, "%04d-%02d-%02d", 1900 + ltm->tm_year, 1 + ltm->tm_mon, ltm->tm_mday);
+    return std::string(date);
+}
+
+// Function to add a new task
+void add_task(WINDOW *input_win, std::map<std::string, std::vector<std::string>>& tasks, std::string CURR_TIME ) {
+    char task[100];
+    wclear(input_win);
+    box(input_win, 0,0);
+    mvwprintw(input_win, 1, 1, "Enter new task: ");  // 将光标移动到输入窗口中的 (1, 1) 位置，并显示 "Enter new task: " 提示用户输入任务。
+    wrefresh(input_win);
+
+   
+    wgetnstr(input_win, task, sizeof(task) - 1);
+   
+    tasks[CURR_TIME].push_back(std::string(task));
+
+    wclear(input_win);
+    // wrefresh(input_win);
+    // delwin(input_win);
+
+}
+
+// Function to display the welcome page
+enum MenuOption show_welcome_page(WINDOW *menu_win, std::vector<std::string> menu) {
+    wclear(menu_win);
+    int choice = 0;
+    
+    box(menu_win,0,0);
+    mvwprintw(menu_win, 1, 1, "Yuzhench Time Schedule");
+    mvwprintw(menu_win, 2, 1, ("Today's Date: " + get_current_date()).c_str());
+    mvwprintw(menu_win, 4, 1, "Options you can choose:");
+
+    int y = 5;
+    for (int i = 0; i < menu.size(); ++i) {
+        mvwprintw(menu_win, y++, 1, "%d. %s", i + 1, menu[i].c_str());
+    }
+
+    mvwprintw(menu_win, y + 1, 1, "Enter your choice (1-%d): ", menu.size());  // 提示用户选择
+
+    wrefresh(menu_win);  // 刷新窗口
+
+    choice = getch() - '0';  // 获取用户输入并转换为数字
+
+    if (choice >= 1 && choice <= menu.size()) {
+        return static_cast<MenuOption>(choice - 1);  // 用户输入有效，退出循环
+    } 
+    else {
+        mvwprintw(menu_win, y + 2, 1, "Invalid choice! Please try again.");  // 无效选择提示
+        wrefresh(menu_win);  // 刷新窗口
+    }
+
+}
+
+
+ 
+
+void show_tasks(WINDOW *task_win, std::map<std::string, std::vector<std::string>>& tasks, std::string CURR_TIME) {
+    wclear(task_win);
+    box(task_win,0,0);
+    mvwprintw(task_win, 1, 1,(get_current_date() + " Tasks:").c_str());
+
+    if (tasks[CURR_TIME].size() == 0){
+        mvwprintw(task_win, 5,1,"--current is empty--");
+    }
+
+    int y = 3;
+
+    mvwprintw(task_win,1,35, "total_taskes: %d", tasks[CURR_TIME].size());
+
+    for (int i = 0; i < tasks[CURR_TIME].size(); ++i) {
+        mvwprintw(task_win, y++, 1, "%d. %s", i + 1, tasks[CURR_TIME][i].c_str());
+    }
+
+    wrefresh(task_win);
+    // getch();  // Wait for user input before returning to menu
+}
+
+void delet_today_tasks (WINDOW *input_element_win, std::map<std::string, std::vector<std::string>>& tasks, std::string CURR_TIME){
+    char password[100];
+    box(input_element_win, 0,0);
+    mvwprintw(input_element_win, 1, 1, "permission checking, please input your password");
+    wmove(input_element_win, 4, 1); 
+    wrefresh(input_element_win);
+
+    wgetnstr(input_element_win, password, sizeof(password) - 1);
+
+    if (std::strcmp(password, PASSWORD) == 0) {
+        tasks[CURR_TIME].clear();
+        wclear(input_element_win);
+        box(input_element_win, 0,0);
+        wrefresh(input_element_win);
+        mvwprintw(input_element_win, 1, 1, "delete all tasks");
+    } else {
+        wclear(input_element_win);
+        box(input_element_win, 0,0);
+        wrefresh(input_element_win);
+        mvwprintw(input_element_win, 1, 1, "you don't have the permission to delete all the tasks");
+    }
+    wrefresh(input_element_win);
+    napms(1000);
+    wclear(input_element_win);
+    // napms(3000);
+    // wrefresh(input_element_win);
+
+}
+
+// Function to delete a task
+void delete_one_task(WINDOW *input_element_win,  std::map<std::string, std::vector<std::string>>& tasks, std::string CURR_TIME) {
+    char task_num[100];
+    box(input_element_win, 0,0);
+    mvwprintw(input_element_win,1, 1, "Delete which task: ");
+    wrefresh(input_element_win);
+
+    wgetnstr(input_element_win, task_num, sizeof(task_num) - 1);
+    int task_index = std::stoi(task_num);
+    
+    if (task_index >= 1 && task_index <= tasks[CURR_TIME].size()) {
+        tasks[CURR_TIME].erase(tasks[CURR_TIME].begin() + task_index - 1);
+        wclear(input_element_win);
+        wrefresh(input_element_win);
+        box(input_element_win, 0,0);
+        mvwprintw(input_element_win,1, 1, ("deleted task: " + std::to_string(task_index)).c_str());
+         
+    } else {
+        wclear(input_element_win);
+        wrefresh(input_element_win);
+        box(input_element_win, 0,0);
+        mvwprintw(input_element_win,1, 1, "you type the wrong task index! ");
+         
+    }
+    wrefresh(input_element_win);
+    napms(1000);
+    wclear(input_element_win);
+    wrefresh(input_element_win);
+}
+
+int main(int argc, char **argv) {
+    std::cout <<"Hellow world" << std::endl;
+    std::map<std::string, std::vector<std::string>> tasks; 
+    tasks["8.27"] = {"task1", "task2"};
+
+    std::vector<std::string> menu = {"Show today tasks", "Add a task", "Remove a task", "Mark as finished", "Mark as unfinished", "Delete one task", "Leave"};
+
+    std::string get_current_date(); 
+    std::string CURR_TIME = get_current_date();
+
+     
+
+
+    // Initialize ncurses
+    // initscr();
+    // clear();
+    // noecho();
+    // cbreak();
+
+
+    //define the size of the window
+    int height = LINES;  
+    int width = COLS / 2;
+
+    // std::cout << "height is: " << height << std::endl;
+     // 定义窗口的宽高
+    int win_height = 10;
+    int win_width = 40;
+
+    // 计算窗口的起始位置，使其居中
+    int starty = (height - win_height) / 2;
+    int startx = (width * 2 - win_width) / 2;
+
+    WINDOW *input_element_win = newwin(win_height, win_width, starty, startx);
+
+    WINDOW *menu_win = newwin(height, width, 0, 0);  
+    WINDOW *task_win = newwin(height, width, 0, width); 
+     
+   
+
+    refresh();
+
+    //ready to get the choice
+    int choice = 0;
+
+    bool running = true;
+
+    //load the file:
+    // load_tasks(tasks, "src/history.txt", CURR_TIME, menu_win);
+
+    // while(running){
+    //     enum MenuOption choice = show_welcome_page(menu_win, menu);  // 显示菜单   
+    //     wrefresh(menu_win);
+
+    //     // load_tasks(tasks, "src/history.txt", CURR_TIME);
+
+
+    //     switch (choice) {
+    //         case ShowTodayTasks:
+    //             show_tasks(task_win, tasks, CURR_TIME);
+    //             wrefresh(task_win);
+    //             break;
+    //         case AddTask:
+    //             add_task(input_element_win, tasks, CURR_TIME);
+    //             wrefresh(input_element_win);
+
+    //             //update the task
+    //             show_tasks(task_win, tasks, CURR_TIME);
+    //             wrefresh(task_win);
+    //             break;
+    //         case RemoveTask:
+    //             delete_one_task(input_element_win, tasks, CURR_TIME);
+    //             wrefresh(menu_win); // 确保 menu_win 在窗口删除前刷新
+    //             show_tasks(task_win, tasks, CURR_TIME);
+    //             wrefresh(task_win); // 确保 task_win 在窗口删除前刷新
+    //             break;
+    //         case MarkAsFinished:
+    //             std::cout << "Marking a task as finished." << std::endl;
+    //             break;
+    //         case MarkAsUnfinished:
+    //             std::cout << "Marking a task as unfinished." << std::endl;
+    //             break;
+    //         case DeleteAll:
+    //             delet_today_tasks(input_element_win,tasks, CURR_TIME);
+    //             refresh();
+    //             wrefresh(menu_win); // 确保 menu_win 在窗口删除前刷新
+    //             show_tasks(task_win, tasks, CURR_TIME);
+    //             wrefresh(task_win); // 确保 task_win 在窗口删除前刷新
+
+    //             // delwin(input_element_win); // 删除窗口
+    //             // input_element_win = nullptr; // 确保指针被设置为nullptr防止悬挂引用
+    //             break;
+    //         case Leave:
+    //             // save_tasks(tasks, "src/history.txt");
+    //             running = false;
+    //             break;
+    //         default:
+    //             std::cout << "Invalid choice." << std::endl;
+    //             break;
+    //     }
+        
+    // }
+     
+    // //  // Refresh to display changes
+    // // refresh();
+
+    // // // Wait for user input before exiting
+    // // getch();
+
+    std::string detail = tasks["8.27"][0];
+    std::cout << "tasks is: " << detail <<std::endl;
+
+    const std::string filePath = std::string(DATA_DIR) + "/history.txt";
+    save_tasks(tasks, filePath);
+    // End ncurses mode
+    endwin();
+
+    
+    
+    return 0;
+}
